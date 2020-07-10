c621ea06f67a817f80f0c3ebc8b4b49b497566e5
caoguojun <96546214@qq.com>
2020-07-10 08:41:15 +0800

    add someting

--- lesson2/simple_http_server.js
+++ lesson2/simple_http_server.js
@@ -3,15 +3,20 @@
 //Schema://host:port/path?query#hash
 
 //port: 22 ssh, 80:http 443:https 27017:mongodb
+//?a=1&b=2&c=3
 const url = `Schema://host:port/path?query#hash`
 const http = require('http')
 const server = http.createServer()
+const qs = require('querystring')
 server.listen(8808)
 //let conter = 0
 server.on('request',(req, res) => {
     // console.log(conter++)
     console.log(req.url)
     const url = req.url
+    const querystring = url.substr(url.indexOf('?')+1, url.length)
+    const query = qs.parse(querystring)
+    console.log(query)
     let responseStr
     if (url === '/hello'){
         responseStr = 'hi there'
